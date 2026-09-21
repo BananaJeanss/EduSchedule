c2b6be7fe44d523df4e5c367660a07c6122a06bb
BananaJeans <18456310+BananaJeanss@users.noreply.github.com>
2026-09-21 10:08:11 +0300

    test(i18n): clean instrumentation imports

--- app/src/androidTest/java/dev/bananajeans/eduschedule/LocalizationInstrumentedTest.kt
+++ app/src/androidTest/java/dev/bananajeans/eduschedule/LocalizationInstrumentedTest.kt
@@ -5,7 +5,6 @@ import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.Locale
 
 @RunWith(AndroidJUnit4::class)
 class LocalizationInstrumentedTest {
